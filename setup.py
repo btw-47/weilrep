e956f2dca9fd320b893462cd5ed657f813b4112d
btw-47 <64304653+btw-47@users.noreply.github.com>
2020-07-13 12:21:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup
 setup(
    name='weilrep',
    version='1.0',
-   description='A useful module',
+   description='Weil representations, Jacobi forms and theta lifts',
    author='Brandon Williams',
    author_email='btw@math.berkeley.edu',
-   packages=['weilrep'],  #same as name
-   install_requires=['sage'], #external packages as dependencies
+   packages=['weilrep'],
 )
