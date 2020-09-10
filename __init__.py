5211b23f3bdf8de0d996994acdf959b1e13a1591
btw-47 <64304653+btw-47@users.noreply.github.com>
2020-09-10 21:54:43 +0200

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from .weilrep.hilbert import HMF, HilbertModularForms, hmf_inputs
 from .weilrep.jacobi_forms_class import jacobi_eisenstein_series, JacobiForms, jf_rankin_cohen, theta_block
-from .weilrep.lifts import jacobian, omf_pivots, omf_rank, omf_relations, orthogonal_eisenstein_series, OrthogonalModularForms, ParamodularForms
+from .weilrep.lifts import jacobian, omf_pivots, omf_rank, omf_relations, orthogonal_eisenstein_series, OrthogonalModularForms
 from .weilrep.lorentz import II, RescaledHyperbolicPlane
+from .weilrep.special import HermitianModularForms, ParamodularForms
 from .weilrep.weilrep import WeilRep
 from .weilrep.weilrep_modular_forms_class import rankin_cohen, smf
